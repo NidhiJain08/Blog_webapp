e650844bec00b4f6720b1f4d24a091d3a2acbf19
NidhiJain08 <munmundangi@gmail.com>
2024-12-22 18:37:07 +0530

    SinglePage and WritePage

--- client/blog_webapp/src/components/Navbar.jsx
+++ client/blog_webapp/src/components/Navbar.jsx
@@ -4,7 +4,26 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
-        <div className='links'></div>
+      <div className='logo'>Blog</div>
+        <div className='links'>
+          <Link className="link" to="/?cat=art">
+          <h6>ART</h6></Link>
+          <Link className="link" to="/?cat=science">
+          <h6>SCIENCE</h6></Link>
+          <Link className="link" to="/?cat=technology">
+          <h6>TECHNOLOGY</h6></Link>
+          <Link className="link" to="/?cat=cinema">
+          <h6>CINEMA</h6></Link>
+          <Link className="link" to="/?cat=design">
+          <h6>DESIGN</h6></Link>
+          <Link className="link" to="/?cat=food">
+          <h6>FOOD</h6></Link>
+          <span>Nidhi</span>
+          <span>Logout</span>
+          <span className="write">
+            <Link className="link" to="/write">Write</Link>
+          </span>
+        </div>
        </div>
     </div>    
   )
